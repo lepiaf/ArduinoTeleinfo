7584030bfebb3ea67f02a5b977bee53c96f9431d
Thierry Thuon <thierry@piaf.eu>
2016-01-31 00:46:49 +0100

    Send more information to gateway

--- ArduinoTeleinfo.ino
+++ ArduinoTeleinfo.ino
@@ -3,19 +3,32 @@
 #include <SPI.h>
 #include "TeleInfo.h"
 
-#define ID 4
+#define NODE_ID 1
+
+#define ID_PAPP 4
+#define ID_IINST 5
+#define ID_HCHC 6
+#define ID_HCHP 7
 
 TeleInfo* homeTeleInfo;
+
 MySensor gw;
-MyMessage msg(ID, V_WATT);
+MyMessage msgPAPP(ID_PAPP, V_WATT);
+MyMessage msgIINST(ID_IINST, V_CURRENT);
+MyMessage msgHCHC(ID_HCHC, V_KWH);
+MyMessage msgHCHP(ID_HCHP, V_KWH);
 
 void setup()
 {
   homeTeleInfo = new TeleInfo();
 
-  gw.begin(NULL, 0);
-  gw.sendSketchInfo("EDF Teleinfo", "1.0");
-  gw.present(ID, S_POWER);
+  gw.begin(NULL, NODE_ID);
+  gw.sendSketchInfo("teleinfo", "1.0");
+
+  gw.present(ID_PAPP, S_POWER);
+  gw.present(ID_IINST, S_POWER);
+  gw.present(ID_HCHC, S_POWER);
+  gw.present(ID_HCHP, S_POWER);
 
   Serial.println(F("Mysensor Node EDF TeleInfo ready"));
 }
@@ -26,15 +39,11 @@ void loop()
   readCompleted = homeTeleInfo->readTeleInfo();
 
   if (readCompleted) {
-    Serial.println(homeTeleInfo->ADC0);
-    Serial.println(homeTeleInfo->IINST);
-    Serial.println(homeTeleInfo->HCHC);
-    Serial.println(homeTeleInfo->HCHP);
-    Serial.println(homeTeleInfo->PAPP);
-    String e = homeTeleInfo->PAPP;
-    int watt = e.toInt();
-    gw.send(msg.set(watt, 1));
+    gw.send(msgIINST.set((homeTeleInfo->IINST).toInt(), 1));
+    gw.send(msgHCHC.set((homeTeleInfo->HCHC).toInt(), 1));
+    gw.send(msgHCHP.set((homeTeleInfo->HCHP).toInt(), 1));
+    gw.send(msgPAPP.set((homeTeleInfo->PAPP).toInt(), 1));
   }
 
-  gw.sleep(1000); //sleep a bit
+  gw.sleep(10000);
 }
