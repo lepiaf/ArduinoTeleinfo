06fdba4467232551814f3e6fd7b164acd764cb36
Thierry Thuon <thierry@piaf.eu>
2016-03-29 18:33:13 +0200

    add soft reset, use low power transmission

--- ArduinoTeleinfo.ino
+++ ArduinoTeleinfo.ino
@@ -1,5 +1,7 @@
 #include <SoftwareSerial.h>
 #include <MySensor.h>
+#include <MyTransportNRF24.h>
+#include <MyHwATMega328.h>
 #include <SPI.h>
 #include "TeleInfo.h"
 
@@ -12,11 +14,16 @@
 
 TeleInfo* homeTeleInfo;
 
-MySensor gw;
+MyTransportNRF24 radio(RF24_CE_PIN, RF24_CS_PIN, RF24_PA_LEVEL_GW);  
+MyHwATMega328 hw;
+MySensor gw(radio, hw);
+
 MyMessage msgPAPP(ID_PAPP, V_WATT);
 MyMessage msgIINST(ID_IINST, V_CURRENT);
 MyMessage msgHCHC(ID_HCHC, V_KWH);
 MyMessage msgHCHP(ID_HCHP, V_KWH);
+
+int count;
 
 void setup()
 {
@@ -31,6 +38,8 @@ void setup()
   gw.present(ID_HCHP, S_POWER);
 
   Serial.println(F("Mysensor Node EDF TeleInfo ready"));
+
+  count = 0;
 }
 
 void loop()
@@ -40,10 +49,25 @@ void loop()
 
   if (readCompleted) {
     gw.send(msgIINST.set(homeTeleInfo->IINST, 0));
+    delay(100);
     gw.send(msgHCHC.set((homeTeleInfo->HCHC).toInt(), 0));
+    delay(100);
     gw.send(msgHCHP.set((homeTeleInfo->HCHP).toInt(), 0));
+    delay(100);
     gw.send(msgPAPP.set((homeTeleInfo->PAPP).toInt(), 0));
   }
 
+  count++;
+
   delay(10000);
+
+  if (count == 60) {
+    softReset();
+    count = 0;
+  }
+
+}
+
+void softReset() {
+  asm volatile ("  jmp 0");
 }
